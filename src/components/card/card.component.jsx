d7d063a2fc612559e66bcfc3388ef1dfca95f796
Hrishabh Athwani <80315612+HrishabhAthwani@users.noreply.github.com>
2022-08-02 03:57:29 -0700

    02-08-2022-1630-FirstCommit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,29 @@
+// import { Component } from "react";
+import './card.style.css'
+
+const Card = ({monster})=>{
+const {id, name, email} = monster;
+    return(
+        <div className="card-container" key={id}>
+            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`Mnster ${name}`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+    </div>
+    )
+}
+
+// class Card extends Component{
+//     render(){
+//         const {name,email,id} = this.props.monster;
+//         return(
+//             <div className="card-container" key={id}>
+//                             <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+//                             <h2>{name}</h2>
+//                             <p>{email}</p>
+//                         </div>
+//         )
+//     }
+// }
+
+
+export default Card;
